7019a3ea5f428dd6aeb1b501d1458c33c6616f5e
fglezm <39835063+fglezm@users.noreply.github.com>
2021-05-23 16:43:58 +0200

    Ejercicio 1 e/s resuelto

--- Ejercicios 3ª Evaluación/src/unidad8_e_s/Ejercicio1.java
+++ Ejercicios 3ª Evaluación/src/unidad8_e_s/Ejercicio1.java
@@ -14,25 +14,24 @@ import java.io.*;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		
+
 		File file;
-		if(args.length == 0) {
-			Scanner s = new Scanner (System.in);
+		if (args.length == 0) {
+			Scanner s = new Scanner(System.in);
 			System.out.print("Ruta: ");
 			file = new File(s.nextLine());
-		}
-		else
+		} else
 			file = new File(args[0]);
 		if (file.exists()) {
 			System.out.println("Nombre: " + file.getName());
 			System.out.print("Tipo: ");
 			if (file.isFile())
-				System.out.println("fichero\nLongitud: "+ file.length() + "bytes");
-				else 
-					System.out.println("directorio\nContiene " + file.list().length+" elementos");
-			System.out.println("Permisos: "+(file.canWrite() ? "escritura ":"")+(file.canRead()?"lectura ":"")+(file.canExecute()?"ejecución":""));
-		}
-		else
+				System.out.println("fichero\nLongitud: " + file.length() + "bytes");
+			else
+				System.out.println("directorio\nContiene " + file.list().length + " elementos");
+			System.out.println("Permisos: " + (file.canWrite() ? "escritura " : "") + (file.canRead() ? "lectura " : "")
+					+ (file.canExecute() ? "ejecución" : ""));
+		} else
 			System.out.println("El fichero o directorio no existe");
 	}
 

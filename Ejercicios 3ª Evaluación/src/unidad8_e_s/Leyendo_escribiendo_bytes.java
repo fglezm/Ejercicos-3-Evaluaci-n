49be24bb9410338811c28f2f959c70b3e3dbcd08
fglezm <39835063+fglezm@users.noreply.github.com>
2021-05-24 08:17:49 +0200

    Escribiendo archivos Byte

--- Ejercicios 3ª Evaluación/src/unidad8_e_s/Leyendo_escribiendo_bytes.java
+++ Ejercicios 3ª Evaluación/src/unidad8_e_s/Leyendo_escribiendo_bytes.java
@@ -21,21 +21,62 @@ public class Leyendo_escribiendo_bytes {
 
 			FileInputStream archivo_lectura = new FileInputStream(
 					"C:/Users/fglezm/Documents/FP/Programación/DSC02332.jpg");
+
 			boolean final_ar = false;
+
 			while (!final_ar) {
+
 				int byte_entrada = archivo_lectura.read();
+
 				if (byte_entrada != -1) // para que no guarde el -1 que indica el final en el array
+
 					datos_entrada[contador] = byte_entrada;
+
 				else
+
 					final_ar = true;
+
 				System.out.println(datos_entrada[contador]);
 
 				contador++;
+
 			}
+
 			archivo_lectura.close();
+
 		} catch (IOException e) {
+
+			System.out.println("Error al acceder a la imagen");
+
 		}
+
 		System.out.println(contador);
+
+		crea_fichero(datos_entrada);
+
+	}
+
+	static void crea_fichero(int datos_nuevo_fichero[]) {
+
+		try {
+
+			FileOutputStream fichero_nuevo = new FileOutputStream(
+					"C:/Users/fglezm/Documents/FP/Programación/DSC02332_copia.jpg");
+
+			for (int i = 0; i < datos_nuevo_fichero.length; i++) {
+
+				fichero_nuevo.write(datos_nuevo_fichero[i]);
+
+			}
+
+			fichero_nuevo.close();
+
+		} catch (IOException e) {
+
+			System.out.println("Error al crear el archivo");
+
+		}
+
 	}
 
 }

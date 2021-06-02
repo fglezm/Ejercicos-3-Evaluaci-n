d190b4ed925a78c591afc4b672fdb18aefd70ccd
fglezm <39835063+fglezm@users.noreply.github.com>
2021-06-02 11:09:06 +0200

    Ejemplo conjuntos

---
+++ Ejercicios 3ª Evaluación/src/unidad8_e_s/MainConjuntos2.java
@@ -0,0 +1,28 @@
+package unidad8_e_s;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * Programa que lee una línea de texto de la entrada estándar y muestra qué caracteres están
+ * repetidos y qué caracteres aparecen una sola vez, ignorando los espacios en blanco
+ */
+public class MainConjuntos2 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Set<Character> repetidos = new HashSet<Character>();
+		Set<Character> sinRepetir = new HashSet<Character>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String linea = br.readLine();
+		for (Character c : linea.toCharArray())
+			if (!Character.isSpaceChar(c) && !repetidos.contains(c) && !sinRepetir.add(c)) {
+				repetidos.add(c);
+				sinRepetir.remove(c);
+
+			}
+		System.out.println("Caracteres sin repetir: " + sinRepetir);
+		System.out.println("Caracteres repetidos " + repetidos);
+	}
+
+}
